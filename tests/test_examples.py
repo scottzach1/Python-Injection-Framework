5f4de22c27962af58f84b88b71636eb7f817954e
Zac Scott <zac.scott11@gmail.com>
2025-02-15 17:17:15 +1300

    test(pytest): parameterized examples tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,4 +1,19 @@
-def test_simple_service():
-    from examples.simple_service import test
+import importlib
+import inspect
+from pathlib import Path
 
-    test.test_main()
+PROJECT_DIR = Path(__file__).parent.parent
+EXAMPLE_DIR = PROJECT_DIR / "examples"
+
+
+def pytest_generate_tests(metafunc):
+    if "example_name" in metafunc.fixturenames:
+        metafunc.parametrize("example_name", [item.name for item in EXAMPLE_DIR.glob("*") if item.is_dir()])
+
+
+def test_example(example_name: str):
+    test_module = importlib.import_module(f"examples.{example_name}.test")
+    test_functions = inspect.getmembers(test_module, lambda x: inspect.isfunction(x) and x.__name__.startswith("test_"))
+
+    for _name, func in test_functions:
+        func()
